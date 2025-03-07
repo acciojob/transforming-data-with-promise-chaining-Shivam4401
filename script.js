2df2d9eae2c74dba88dfb952f848620c529d5416
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-07 15:40:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("btn").addEventListener("click",function(){
 	
 	const n = parseFloat(document.querySelector("#ip").value);
 	let outputDiv = document.getElementById("output");
-	// outputDiv.textContent = "";
+	outputDiv.textContent = "";
 
 const initialPromise = new Promise ((resolve,reject) => {
    
